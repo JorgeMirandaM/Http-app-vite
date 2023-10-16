c39afbab3e1f0d1354452c65109b9d967cc6b566
JorgeMirandaM <jorge100099hotmail.com>
2023-10-16 17:05:08 -0600

    show a simple http request finished

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,10 +1,39 @@
 
+/**
+ * @returns {Promise<Object>}
+ */
+const fetchQuote = async () => {
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    return data[0];
+}
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp=(element)=>{
-    document.querySelector('#app-title').innerHTML='Breakingbad App';
-    element.innerHTML='Loading...';
+export const BreakingbadApp = async (element) => {
+    document.querySelector('#app-title').innerHTML = 'Breakingbad App';
+    element.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerHTML = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel,authorLabel,nextQuoteButton);
+    };
+
+    fetchQuote()
+        .then(data => renderQuote(data));
+
+
+    nextQuoteButton.addEventListener('click',()=>{
+        element.innerHTML = 'Loading...';
+        fetchQuote()
+        .then(data => renderQuote(data));
+    });
 }
