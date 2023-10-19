34f866b3f0340901950c34eae19e1f0236e4972d
JorgeMirandaM <jorge100099hotmail.com>
2023-10-18 20:10:52 -0600

    update user in table function implemented

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -1,3 +1,4 @@
+import { User } from "../models/user";
 import { loadUsersByPage } from "../use-cases/load-users-by-page"
 
 
@@ -21,8 +22,26 @@ const loadPreviousPage= async ()=>{
 
 }
 
-const onUserChanged = ()=>{
+/**
+ * 
+ * @param {User} user 
+ */
+const onUserChanged = (updatedUser)=>{
 
+    let wasFound= false;
+
+    state.users= state.users.map(user =>{
+        if( user.id === updatedUser.id){
+            wasFound=true;
+            return updatedUser;
+        }
+
+        return user;
+    });
+
+    if(state.users.length <10 && !wasFound){
+        state.users.push(updatedUser);
+    }
 }
 
 const reloadPage= async ()=>{
